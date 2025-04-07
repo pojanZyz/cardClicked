4314a59b5cda8e7e19c1d94d485d5fac058d5f7c
pojanZyz <akunword0@gmail.com>
2025-04-07 16:59:26 +0700

    fix

--- src/router/cardRouter.js
+++ src/router/cardRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import upload from "../config/multer.js";
+import upload from "../middleware/uploadFile.js";
 import createCard from "../controller/cardController.js";
 
 const router = express.Router();
