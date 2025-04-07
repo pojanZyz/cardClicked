29ef32adb1a7c8bc3759ea8d08bb69a064aa5f51
pojanZyz <akunword0@gmail.com>
2025-04-07 16:08:20 +0700

    add

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/card', cardRouter);
+app.use('/', (req, res) => {
+    res.send('Welcome to the Card API!');
+});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running on port ${process.env.PORT || 3000}`);
